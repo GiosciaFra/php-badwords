9c97d79e51aced205afd8e8d29aa668b7b711d21
giosciafra <francescogioscia8@gmail.com>
2024-03-15 17:42:05 +0100

    creato basic form e file result, con i parametri

--- index.php
+++ index.php
@@ -13,11 +13,24 @@
 
     <?php
 
-    $name = "Matteo";
+    ini_set('display_errors', 1);
 
-    echo "ciao sono " . $name
+    ini_set('display_startup_errors', 1);
 
-        ?>
+    error_reporting(E_ALL);
+
+
+
+    ?>
+
+    <h2>Inserisci il tuo paragrafo e la relativa parola da censurare:</h2>
+    <form action="result.php" method="GET">
+        <label for="paragraph">Paragrafo:</label><br>
+        <textarea id="paragraph" name="paragraph"></textarea><br>
+        <label for="bad_word">Parola da censurare:</label><br>
+        <input type="text" id="bad_word" name="bad_word"><br><br>
+        <input type="submit" value="Invia">
+    </form>
 
 </body>
 
