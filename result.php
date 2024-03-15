904cf0bac9f04ebaa015535ded6dfa547b18035f
giosciafra <francescogioscia8@gmail.com>
2024-03-15 18:09:21 +0100

    add: stampa 'result' in pagina wip: style

--- result.php
+++ result.php
@@ -7,17 +7,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>php-badwords</title>
 </head>
 
 <body>
 
+    <h2>Risultato:</h2>
+
     <?php
 
-    $paragrafo = $_GET['paragraph'];
+    $paragraph = $_GET['paragraph'];
     $bad_word = $_GET['bad_word'];
 
+    // Stampo il paragrafo e la sua lunghezza
+    echo "<p><strong>Paragrafo:</strong> $paragraph</p>";
+    echo "<p><strong>Lunghezza del paragrafo:</strong> " . strlen($paragraph) . "</p>";
+
+    // Sostituisco la parola da censurare con ***
+    $censored_paragraph = str_replace($bad_word, '***', $paragraph);
+
+    // Stampo il paragrafo censurato e la sua lunghezza
+    echo "<p><strong>Paragrafo Censurato:</strong> $censored_paragraph</p>";
+
+    echo "<p><strong>Lunghezza del paragrafo censurato:</strong> " . strlen($censored_paragraph) . "</p>";
+
     ?>
+
+
 
 </body>
 
