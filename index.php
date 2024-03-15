7a52e308e67adfd55aae727dfbb24a3a911270b7
giosciafra <francescogioscia8@gmail.com>
2024-03-15 19:23:45 +0100

    feat: esercizio stilizzato e finito

--- index.php
+++ index.php
@@ -5,11 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-badwords</title>
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
-<body>
+<body class=" bg-dark text-white-50 ">
 
-    <h1>Ciao</h1>
 
     <?php
 
@@ -23,14 +26,32 @@
 
     ?>
 
-    <h2>Inserisci il tuo paragrafo e la relativa parola da censurare:</h2>
-    <form action="result.php" method="GET">
-        <label for="paragraph">Paragrafo:</label><br>
-        <textarea id="paragraph" name="paragraph"></textarea><br>
-        <label for="bad_word">Parola da censurare:</label><br>
-        <input type="text" id="bad_word" name="bad_word"><br><br>
-        <input type="submit" value="Invia">
-    </form>
+    <div
+        class="container d-flex justify-content-center flex-column align-items-center text-center py-5 my-5 border border-5 border-danger rounded-5   ">
+
+
+        <h1 class="pb-4">Inserisci il tuo paragrafo e la relativa parola da censurare</h1>
+        <form action="result.php" method="GET">
+
+            <div class=" border border-4 border-danger rounded-4 p-4 mb-3 ">
+                <label for="paragraph" class="mb-2">Paragrafo:</label><br>
+                <textarea id="paragraph" name="paragraph" class="mb-4"></textarea><br>
+            </div>
+
+
+            <div class=" border border-4 border-danger rounded-4 p-4 mb-3 ">
+                <label for="bad_word" class="mb-2">Parola da censurare:</label><br>
+                <input type="text" id="bad_word" name="bad_word" class="mb-4"><br>
+            </div>
+
+            <input type="submit" value="Invia" class="btn btn-danger ">
+        </form>
+    </div>
+
+    <!-- bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 
 </body>
 
--- result.php
+++ result.php
@@ -1,5 +1,12 @@
 <!DOCTYPE html>
 
+<?php
+
+$paragraph = $_GET['paragraph'];
+$bad_word = $_GET['bad_word'];
+
+$censored_paragraph = str_replace($bad_word, '***', $paragraph);
+?>
 
 
 <html lang="en">
@@ -8,33 +15,61 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-badwords</title>
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
-<body>
+<body class=" bg-dark text-white-50">
 
-    <h2>Risultato:</h2>
-
-    <?php
-
-    $paragraph = $_GET['paragraph'];
-    $bad_word = $_GET['bad_word'];
-
-    // Stampo il paragrafo e la sua lunghezza
-    echo "<p><strong>Paragrafo:</strong> $paragraph</p>";
-    echo "<p><strong>Lunghezza del paragrafo:</strong> " . strlen($paragraph) . "</p>";
-
-    // Sostituisco la parola da censurare con ***
-    $censored_paragraph = str_replace($bad_word, '***', $paragraph);
-
-    // Stampo il paragrafo censurato e la sua lunghezza
-    echo "<p><strong>Paragrafo Censurato:</strong> $censored_paragraph</p>";
-
-    echo "<p><strong>Lunghezza del paragrafo censurato:</strong> " . strlen($censored_paragraph) . "</p>";
-
-    ?>
+    <div class=" container d-flex justify-content-center flex-column align-items-center text-center py-5 mt-2 border
+    border-5 border-danger rounded-5 ">
 
 
+        <h1 class="pb-4 ">Risultato</h1>
+
+
+
+        <!-- Stampo il paragrafo e la sua lunghezza -->
+        <p class=" border border-4 border-danger rounded-4 p-4">
+            <strong>Paragrafo:</strong><br>
+
+            <?php echo $paragraph ?>
+
+        </p>
+
+
+        <p class=" border border-4 border-danger rounded-4 p-4">
+
+            <strong>Lunghezza del paragrafo:</strong><br>
+            <?php echo strlen($paragraph) ?>
+        </p>
+
+
+        <!-- Stampo il paragrafo censurato e la sua lunghezza -->
+
+        <p class=" border border-4 border-danger rounded-4 p-4">
+
+            <strong>Paragrafo Censurato:</strong><br>
+            <?php echo $censored_paragraph ?>
+        </p>
+
+        <p class=" border border-4 border-danger rounded-4 p-4">
+
+            <strong>Lunghezza del paragrafo censurato:</strong><br>
+            <?php echo strlen($censored_paragraph) ?>
+        </p>
+
+    </div>
+
+
+    <!-- bootstrap -->
+    <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+        </script>
 
 </body>
+
 
 </html>
